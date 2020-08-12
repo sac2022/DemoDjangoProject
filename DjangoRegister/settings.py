c7ca2876dd638679eed1947437643cc9be9cb8e1
sac2022 <sac2022@users.noreply.github.com>
2020-08-12 19:39:43 +0530

    Added files and changes required for deployment to heroku

--- DjangoRegister/settings.py
+++ DjangoRegister/settings.py
@@ -121,3 +121,9 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
 ]
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
